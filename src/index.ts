e074cb776feb57d18ed44f0469c7eb1ec0759b56
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-06-18 22:09:45 -0500

    Agregando conexión con Mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import colors from 'colors';
 import server from "./server"
 
 
 const port = process.env.PORT || 4000;
  server.listen(port, ()=>{
-     console.log("Server Corriendo en el puerto:", port)
+     console.log(colors.blue.bold("Server Corriendo en el puerto:"), port)
  })
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';//ESM 
+import 'dotenv/config'
 import router from './router';
+import { connectDB } from './db/config';
+
 const app= express();
+
+connectDB();
 
 //Habilitar leer datos
 app.use(express.json())
