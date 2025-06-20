9eee45e9b815c4790fedb0e3c9f84e14a6ef7287
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-06-19 20:24:38 -0500

    Agregando modelo de usuarios

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
+import User from "./models/User";
 
 const router = Router();
 
 /**Autenticación y registro */
 
-router.post('/auth/register', (req,res)=>{
-    console.log("Desde Register")
+router.post('/auth/register', async(req,res)=>{
+
+    await User.create(req.body);
 })
 
 export default router;
