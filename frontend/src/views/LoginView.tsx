97f1cd9f585cee5666eea9b6e04a9963a85d3580
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-07-09 12:44:04 -0500

    Agregando validación a los form

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -2,9 +2,13 @@ import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
     <>
-   
+   <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+
     <nav>
-       <Link to="/auth/register">
+       <Link 
+       to="/auth/register"
+       className="text-center text-white text-lg block"
+       >
        ¿No tienes cuenta? Registrate
        </Link>
     </nav>
