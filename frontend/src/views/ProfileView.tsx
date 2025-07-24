6c42da676f2279e81c30af8133f61c1f00fe4ab5
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-07-24 17:20:15 -0500

    Terminando update Profile

--- frontend/src/views/ProfileView.tsx
+++ frontend/src/views/ProfileView.tsx
@@ -1,21 +1,35 @@
 import {useForm} from 'react-hook-form'
-import { useQueryClient } from '@tanstack/react-query';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
 import ErrorMessage from '../components/ErrorMessage';
 import type { ProfileForm, User } from '../types';
+import { updateProfile } from '../api/DevTreeAPI';
 
 export default function ProfileView() {
     const queryClient = useQueryClient()
     const data : User = queryClient.getQueryData(['data'])!
 
-    console.log(data)
 
     const { register, handleSubmit, formState:{errors}} = useForm<ProfileForm>({defaultValues:{
         handle: '',
         description: ''
     }});
 
+    const updateProfileMutation = useMutation({
+        mutationFn: updateProfile,
+        onError: (error) => {
+            toast.error(error.message)
+        },
+        onSuccess: (data) =>{
+            toast.success(data)
+            queryClient.invalidateQueries({queryKey:['user']})
+        }
+    })
+
     const handleUserProfileForm = (formData: ProfileForm) =>{
         console.log(formData)
+
+        updateProfileMutation.mutate(formData)
     }
     return (
         <form 
