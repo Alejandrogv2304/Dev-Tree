4c7c51ab5295755a9f22e24f60438d120b727d0e
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2025-07-29 21:51:44 -0500

    Agregando subida de imagenes

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -7,6 +7,7 @@ export interface IUser extends Document{
     email:string
     password:string
     description:string
+    image:string
 }
 const userSchema = new Schema({
     handle :{
@@ -35,7 +36,10 @@ const userSchema = new Schema({
     },
     description:{
     type:String,
-    required:false,
+    default:''
+    },
+    image:{
+    type:String,
     default:''
     }
 })
